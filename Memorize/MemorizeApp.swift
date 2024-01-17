8f71a21c3fb28abd50c3c1d72715c7305a166d47
Chris Lantier <chrisj17@gmail.com>
2024-01-16 21:54:19 -0500

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Chris Lantier on 1/16/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
